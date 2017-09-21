f6a44b49b5d06209c681ffa654612531aa5e1c08
Dora Megyesi <dora.megyesi@gmail.com>
2017-09-21 16:19:09 +0200

    can search for profiles and open them

--- githubfinder/js/main.js
+++ githubfinder/js/main.js
@@ -9,7 +9,21 @@ $(document).ready(function() {
                 client_secret: "16774162bc20d997c284185ecfddcf96e05c6ef2"
             }
         }).done(function(user) {
-            console.log(user);
+            $("#profile").html(`
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">${user.name}</h3>
+                    </div>
+                    <div class="panel-body">
+                        <div class="row">
+                            <div class="col-md-3">
+                                <img class="thumbnail avatar" src="${user.avatar_url}">
+                                <a target="_blank" class="btn btn-primary btn-block" href="${user.html_url}">View profile</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>`
+            );
         });
     });
 });
